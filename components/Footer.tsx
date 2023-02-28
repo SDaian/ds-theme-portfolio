2f209a542d553276ed10be4a4b10d82b7d87f860
Daian Scuarissi <scuarissid@gmail.com>
2023-02-28 22:44:32 +0100

    fix: Accesibility issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,16 @@ const Footer = () => {
     <footer className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl'>
       <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
       <div className='mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
-        <div className='text-neutral-500 dark:text-neutral-100'>© {new Date().getFullYear()} Daian Scuarissi</div>
+        <div className='text-neutral-500 dark:text-neutral-100'>
+          © {new Date().getFullYear()} Daian Scuarissi
+        </div>
         <div className='flex flex-row items-center justify-center space-x-2 mb-1'>
-          <a href='https://github.com/SDaian' target='_blank' rel='noreferrer'>
+          <a
+            href='https://github.com/SDaian'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Github'
+          >
             <AiOutlineGithub
               className='inline-block w-6 h-6 mx-2 text-neutral-500 hover:text-neutral-600 dark:text-neutral-100'
               size={30}
@@ -21,6 +28,7 @@ const Footer = () => {
             href='https://twitter.com/SDaian'
             target='_blank'
             rel='noreferrer'
+            aria-label='Twitter'
           >
             <AiOutlineTwitter
               className='inline-block w-6 h-6 mx-2 text-neutral-500 hover:text-neutral-600 dark:text-neutral-100'
@@ -31,6 +39,7 @@ const Footer = () => {
             href='https://www.linkedin.com/in/daian-scuarissi/'
             target='_blank'
             rel='noreferrer'
+            aria-label='Linkedin'
           >
             <AiOutlineLinkedin
               className='inline-block w-6 h-6 mx-2 text-neutral-500 hover:text-neutral-600 dark:text-neutral-100'
