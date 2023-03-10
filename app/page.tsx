da840ba6a69a550a25be3d708c4fa19232c5bf0f
Daian Scuarissi <scuarissid@gmail.com>
2023-03-10 20:36:17 +0100

    feat: Change Metadata SEO + Change favicon + Adding Experience Section (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
-import AboutSection from "@/components/AboutSection";
-import ContactSection from "@/components/ContactSection";
-import HeroSection from "@/components/HeroSection";
+import { Metadata } from 'next';
+
+import AboutSection from '@/components/AboutSection';
+import ContactSection from '@/components/ContactSection';
+import ExperienceSection from '@/components/ExperienceSection';
+import HeroSection from '@/components/HeroSection';
+
+export const metadata: Metadata = {
+  title: 'Daian Scuarissi | Software Engineer | Frontend Developer',
+  description:
+    'Software Engineer based in Madrid, Spain. Turning ideas into visually stunning and performant web applications using Angular or React.',
+  keywords: [
+    'personal website',
+    'daian scuarissi',
+    'angular',
+    'react',
+    'software engineer',
+    'frontend development',
+    'contact me',
+  ],
+  openGraph: {
+    title: 'Daian Scuarissi - Software Engineer',
+    siteName: 'Daian Scuarissi - Software Engineer',
+  },
+  alternates: {
+    canonical: 'https://daian-scuarissi.vercel.app/'
+  }
+};
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+    <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl'>
       <HeroSection />
       <AboutSection />
+      <ExperienceSection />
       <ContactSection />
     </main>
-  )
+  );
 }
---
+++ components/ResumeButton.tsx
@@ -0,0 +1,24 @@
+import { HiArrowDownTray } from 'react-icons/hi2';
+
+export const ResumeButton = () => {
+  return (
+    <section className='mt-12 flex items-center justify-center'>
+      <a
+        href='/Resume-Daian_Scuarissi.pdf'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        {/* <button className='buttonResume overflow-hidden relative cursor-pointer p-8 w-80 h-8 rounded-2xl shadow-lg flex items-center justify-center'></button> */}
+        <button
+          type='button'
+          data-te-ripple-init
+          data-te-ripple-color='light'
+          className='w-80 h-12 flex items-center justify-center rounded-xl bg-blue-500 px-6 pt-2.5 pb-2 text-lg font-semibold leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg'
+        >
+          <HiArrowDownTray className='mr-2 h-6 w-6' />
+          Download Resume
+        </button>
+      </a>
+    </section>
+  );
+};
