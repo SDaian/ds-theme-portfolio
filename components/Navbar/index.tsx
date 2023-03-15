22c5bfd0eb338acf008ec77f41326bed6a724840
Daian Scuarissi <scuarissid@gmail.com>
2023-03-15 10:40:10 +0100

    feat: Fixes on dark mode (#10)

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className='w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow backdrop-blur-md'>
+    <header className='w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow backdrop-blur-md dark:shadow-slate-500'>
       <div className='md:max-w-[1200px] md:mx-auto'>
         <div className='justify-between md:items-center md:flex'>
           <Logo navbar={navbar} setNavbar={setNavbar} />
