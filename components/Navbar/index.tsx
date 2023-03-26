c000aa4108e491fd040ba29453d44a84ade7293a
Daian Scuarissi <scuarissid@gmail.com>
2023-03-27 01:53:07 +0200

    feat: Add social on navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import { NavItems } from './Components/NavItems';
 import { Logo } from './Components/Logo';
+import { SocialIcons } from '../Shared/SocialIcons';
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
@@ -10,8 +11,11 @@ const Navbar = () => {
   return (
     <header className='w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow backdrop-blur-md dark:shadow-slate-500'>
       <div className='md:max-w-[1200px] md:mx-auto'>
-        <div className='justify-between md:items-center md:flex'>
+        <div className='relative justify-between md:items-center md:flex'>
           <Logo navbar={navbar} setNavbar={setNavbar} />
+          <div className='hidden lg:block absolute left-[43%]'>
+            <SocialIcons className={'text-black'} />
+          </div>
           <NavItems navbar={navbar} />
         </div>
       </div>
---
+++ components/Shared/SocialIcons/Data/SocialItems.ts
@@ -0,0 +1,19 @@
+import { SocialItem } from '../Models/SocialItem';
+
+export const SocialItems: Array<SocialItem> = [
+  {
+    name: 'Github',
+    link: 'https://github.com/SDaian',
+    icon: 'AiOutlineGithub',
+  },
+  {
+    name: 'Linkedin',
+    link: 'https://www.linkedin.com/in/daian-scuarissi/',
+    icon: 'AiOutlineLinkedin',
+  },
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/ASDaian',
+    icon: 'AiOutlineTwitter',
+  },
+];
